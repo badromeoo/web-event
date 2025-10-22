2f2d3d1af809e96e3151c9aad3ee3927172be6b9
bintang <astawabintang@gmail.com>
2025-10-22 09:07:29 +0800

    rename frontend

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,4 +1,5 @@
-import {Router} from 'express';
+import {Router, type Request, type Response} from 'express';
+import jwt from 'jsonwebtoken'
 import prisma from "../lib/prisma.ts";
 import bcrypt from 'bcrypt';
 
@@ -43,5 +44,55 @@ router.post("/register", async (req, res) => {
     }
 });
 
+router.post('/login', async (req: Request, res: Response) => { 
+  try {
+    const { email, password } = req.body; 
+
+    
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email dan password dibutuhkan' }); 
+    }
+
+    
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Email atau password salah' }); 
+    }
+
+    
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Email atau password salah' }); 
+    }
+
+    
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        role: user.role,
+        email: user.email,
+      },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '1d' }
+    );
+
+    
+    res.status(200).json({ 
+      message: 'Login berhasil',
+      token: token,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Terjadi kesalahan di server' }); 
+  }
+});
+
 export default router;
+
+
 
